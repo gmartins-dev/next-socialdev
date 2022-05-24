ded1a6598fdd23983496aa46f13363b71b3efe6f
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-05-24 08:06:54 -0300

    styled components settings

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,23 @@
-import '../style.css'
+import { createGlobalStyle } from 'styled-components';
 
-function App ({ Component, pageProps }) {
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
+function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
-  )
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default App
+export default App;
